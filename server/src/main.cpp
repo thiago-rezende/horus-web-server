2a2f1af2c7a5a5e3b3170741538cf790da802840
Thiago Rezende <horus.he4rt@gmail.com>
2021-06-25 15:21:09 -0300

    feat: added startup options

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -9,6 +9,8 @@
  *
  */
 
+#include <cxxopts.hpp>
+
 #include "utils/platform_detection.hpp"
 #include "utils/path.hpp"
 #include "network.hpp"
@@ -17,15 +19,29 @@
 
 int main(int argc, char **argv)
 {
-    horus::logger::init();
+    cxxopts::Options options("horus-web-server", "Simple and (not yet) configurable web server written in C++");
 
-    horus::logger::info("Horus Web Server {}", "v0.1.0");
+    options.add_options("", {{"port", "port number", cxxopts::value<uint16_t>()->default_value("8080")},
+                             {"doc", "document root", cxxopts::value<std::string>()->default_value((horus::utils::get_executable_dir().parent_path() / "share" / "horus-www").string())},
+                             {"h,help", "print help"}});
 
     try
     {
+        cxxopts::ParseResult options_result = options.parse(argc, argv);
+
+        if (options_result.count("help"))
+        {
+            std::cout << options.help() << std::endl;
+            return 0;
+        }
+
+        horus::logger::init();
+
+        horus::logger::info("Horus Web Server {}", "v0.1.0");
+
         asio::io_context context;
 
-        std::filesystem::path document_root = horus::utils::get_executable_dir().parent_path() / "share" / "horus-www";
+        std::filesystem::path document_root = options_result["doc"].as<std::string>();
 
         bool document_root_exists = std::filesystem::exists(document_root);
 
@@ -37,7 +53,7 @@ int main(int argc, char **argv)
 
         horus::logger::info("[server] document root: {}", document_root.string());
 
-        horus::server server(context, 8080, document_root);
+        horus::server server(context, options_result["port"].as<uint16_t>(), document_root);
         server.run();
     }
     catch (const std::exception &e)
